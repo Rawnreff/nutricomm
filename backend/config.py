9ad19584d8644b334cb49891eaaf25fc25b33918
Rafen Sidiq Anggara <134561537+Rawnreff@users.noreply.github.com>
2025-10-23 15:30:55 +0700

    backend

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ load_dotenv()
 
 class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/nutricomm")
-    MQTT_BROKER_URL = os.getenv("MQTT_BROKER_URL", "localhost")
+    MQTT_BROKER_URL = os.getenv("MQTT_BROKER_URL", "10.218.22.169")
     MQTT_BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT", 1883))
-    MQTT_TOPIC = os.getenv("MQTT_TOPIC", "nutricomm/#")
+    MQTT_TOPIC = os.getenv("MQTT_TOPIC", "nutricomm/sensor")
